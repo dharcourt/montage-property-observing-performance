0d77260dc707fe72173a711405c7fa68c90a26e0
Charles d'Harcourt <noreply>
2015-05-28 02:33:25 +0000

    Initial test

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -8,9 +8,41 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Main = Component.specialize(/** @lends Main# */ {
+
     constructor: {
         value: function Main() {
             this.super();
+            this._a = 1;
+            this._b = 42;
+        }
+    },
+    
+    a: {
+        set: function (a) {
+            console.log("setA(" + a + ")");
+            this._a = a
+        },
+        get: function () {
+            console.log("getA() = " + this._a);
+            return this._a;
+        }
+    },
+    
+    b: {
+        set: function (b) {
+            console.log("setB(" + b + ")");
+            this._b = b
+        },
+        get: function () {
+            console.log("getB() = " + this._b);
+            return this._b;
+        }
+    },
+
+    handleButtonAction: {
+        value: function (event) {
+            this.a = this.a + 5;
         }
     }
+
 });
